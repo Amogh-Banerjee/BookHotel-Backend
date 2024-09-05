15b867cbc87f23801f2598d510e2104458f0cf54
Amogh-Banerjee <amoghbanerjee08@gmail.com>
2024-09-05 18:36:26 +0530

    Initial commit

---
+++ HotelDetailsAPI/Models/HotelDetails.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.Text.Json.Serialization;
+
+namespace HotelDetailsAPI.Models
+{
+	public class HotelDetails
+	{
+		public int _id {  get; set; }
+		public string title { get; set; }
+		public string rating {  get; set; }
+		public string city {  get; set; }
+		public string location {  get; set; }
+		public Score score { get; set; }
+		public List<string> amenities { get; set; }
+		public int price {  get; set; }
+
+	}
+
+	public class Score
+	{
+		public decimal scorenum {  get; set; }
+		public string scoreDesc {  get; set; }
+		public int n_ratings {  get; set; }
+	}
+}
---
+++ HotelDetailsAPI/Services/HotelDetailsService.cs
@@ -0,0 +1,40 @@
+﻿using HotelDetailsAPI.Models;
+using MongoDB.Driver;
+
+namespace HotelDetailsAPI.Services
+{
+	public class HotelDetailsService
+	{
+		private readonly IMongoCollection<HotelDetails> _hotelDetailsCollection;
+
+		public HotelDetailsService(IMongoDatabase database)
+		{
+			_hotelDetailsCollection = database.GetCollection<HotelDetails>("HotelDetails");
+		}
+
+		public async Task<HotelDetails> GetHotelByIdAsync(int id)
+		{
+			return await _hotelDetailsCollection.Find(hotel => hotel._id == id).FirstOrDefaultAsync();
+		}
+
+		public async Task<List<HotelDetails>> GetAllHotelsAsync()
+		{
+			return await _hotelDetailsCollection.Find(hotel => true).ToListAsync();
+		}
+
+		public async Task CreateHotelAsync(HotelDetails hotel)
+		{
+			await _hotelDetailsCollection.InsertOneAsync(hotel);
+		}
+
+		public async Task UpdateHotelAsync(int id, HotelDetails updatedHotel)
+		{
+			await _hotelDetailsCollection.ReplaceOneAsync(hotel => hotel._id == id, updatedHotel);
+		}
+
+		public async Task DeleteHotelAsync(int id)
+		{
+			await _hotelDetailsCollection.DeleteOneAsync(hotel => hotel._id == id);
+		}
+	}
+}
